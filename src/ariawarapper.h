661b5b0301f4ef7156c5c42f4da8faf851d200d0
born-jre <bornjre@gmail.com>
2017-10-31 02:12:55 +0545

    added slots signal for handeling global download and other inf into status bar NEEDS CLEANUP DOn't remeber

--- src/ariawarapper.h
+++ src/ariawarapper.h
@@ -13,15 +13,16 @@ public:
     explicit ariawarapper(QObject *parent=0);
     aria2::Session* session;
     aria2::SessionConfig config;
-    std::vector<std::string> url;
     aria2::KeyVals options;
     aria2::GlobalStat gstat;
 
 signals:
-
+    void globalDownloadStat(int inactive, int active, int gdl, int gup);
+    void dowlodStatPerItem(int id, int completed, int total,int perDl, int perUp);
 
 public slots:
     void update();
+    void addNewDownload(/*aria2::A2Gid *id,*/ QString url);
 };
 
 #endif // ARIAWARAPPER_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,14 +5,20 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-
       ui->setupUi(this);
       dl_handle = new ariawarapper;
       dl_handle->moveToThread(&worker);
       worker.start();
       timer = new QTimer(this);
+      label = new QLabel(this); //just temp
+      label->setText("Hello form QT");
+
       connect(timer, SIGNAL(timeout()), dl_handle, SLOT(update()));
+      connect(dl_handle,&ariawarapper::globalDownloadStat,this,&MainWindow::globalDownloadStat);
+      connect(dl_handle,&ariawarapper::dowlodStatPerItem,this,&MainWindow::dowlodStatPerItem);
+      connect(this,&MainWindow::addNewDownload, dl_handle,&ariawarapper::addNewDownload);
       timer->start(1000);
+      std::cout<<"Failed";
       //emit update();
 
 }
@@ -25,6 +31,24 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionAddNew_triggered()
 {
-    newDialog = new addNewDialog;
-    newDialog->show();
+    //newDialog = new addNewDialog;
+    //newDialog->show();
+    QString url = "http://ipv4.download.thinkbroadband.com/5MB.zip";
+    aria2::A2Gid id;
+    emit addNewDownload(/*&id,*/url);
+
+
+
+}
+void MainWindow::globalDownloadStat(int inactive, int active, int gdl, int gup)
+{
+    QString message = QString("Inactive:%1 Active:%2 Speed down:%3KB/s up:%4KB/s").arg(inactive).arg(active).arg(gdl/1024).arg(gup/1024);
+    statusBar()->showMessage(message);
+}
+void MainWindow::dowlodStatPerItem(int id, int completed, int total,int perDl, int perUp)
+{
+    QString message = QString("ID Downloaded:%2/%3 Speed D%4KB/s U%4KB/s").arg(completed).arg(total).arg(perDl).arg(perUp);
+    label->setText(message);
+    ui->verticalLayout_2->addWidget(label);
+    std::cerr<<"for love of world";
 }
